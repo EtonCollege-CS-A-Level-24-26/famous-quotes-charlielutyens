de081a2758946083f859a3da3784b2a70fc002af
Charlie Lutyens-Humfrey <lutyens-humfrey.c@etoncollege.org.uk>
2025-03-23 13:28:08 +0000

    stuck on delete functin

--- FamousQuotesParse/View/FamousQuotesView.swift
+++ FamousQuotesParse/View/FamousQuotesView.swift
@@ -15,11 +15,36 @@ struct ContentView: View {
             List {
                 ForEach(vm.quotes) { quote in
                     Text(quote.content)
+                        .swipeActions {
+                            Button(action: {
+                                QuoteRepository.shared.deleteQuote(quote: quote)
+                            }) {
+                                Image(systemName: "trash")
+                            }
+                            .tint(.red)
+                            
+                            Button(action: {
+                                vm.isShowingEditQuote = true
+                            }) {
+                                Image(systemName: "pencil")
+                            }
+                            .tint(.yellow)
+                        }
+                        .sheet(isPresented: $vm.isShowingEditQuote) {
+                            Form {
+                                TextField("Author", text: $vm.editQuoteAuthor)
+                                TextField("Quote", text: $vm.editQuoteContent)
+                                Button("Add") {
+                                    QuoteRepository.shared.editQuote(quote: quote, editedAuthor: vm.editQuoteAuthor, editedContent: vm.editQuoteContent)
+                                }
+                            }
+                        }
                 }
             }
             .onAppear {
                 vm.restoreQuotes()
             }
+            
             .sheet(isPresented: $vm.isShowingAddQuote) {
                 Form {
                     TextField("Author", text: $vm.newQuoteAuthor)
